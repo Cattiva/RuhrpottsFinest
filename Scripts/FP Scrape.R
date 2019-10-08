54ada0b40c3a209878cce12d21b57b6daabf5d88
Cattiva <alexander.nickel@mailbox.org>
2019-10-08 12:44:53 +0000

    try

--- Scripts/FP Scrape.R
+++ Scripts/FP Scrape.R
@@ -1,16 +1,44 @@
 library(tidyverse)
 library(rvest)
 library(jsonlite)
+library(RVerbalExpressions)
 
 #Aim of this script is to scrape available / taken players from either Fantasypros.com or NFL.com
 
-url <- "https://www.fantasypros.com/nfl/rankings/ros-overall.php?print=true"
+url <- "https://www.fantasypros.com/nfl/rankings/ros-overall.php"
 
-raw <- read_html(url)
+raw <- read_html(url) %>% 
+  html_table(fill = TRUE)
 
-name <- raw %>% html_node(".player-label sticky-cell sticky-cell-two")
+player_db <- raw[[1]] %>% 
+  as_tibble(.name_repair = "unique") %>% 
+  select(c(Rank:ADP), -WSIT)
+
+x <- rx_uppercase() %>% 
+  rx_find(". ")
+
+player_db_test<- player_db %>% 
+  mutate(name = player_db$`Overall (Team)` %>% str_split_fixed(x, n = 2))
+
+  
+player_db$`Overall (Team)` %>% 
+  str_split_fixed(x, n = 2) %>% 
+  as_tibble(.name_repair = "minimal")
 
 
+player_db <- player_db %>% 
+  rename("name" = `Overall (Team)`) 
+
+player_db$name %>% 
+  unnest()
+
+player_d<- player_db$`Overall (Team)` %>% 
+  str_split_fixed(x, 2) %>% 
+  as_tibble() %>% 
+  rename("name" = V1,
+         "other" = V2)
+
+split[1]
 
 from_json_to_tibble <- function(json) {
   json %>%
